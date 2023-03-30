72cd3f97f83538eaf8969137b55c4cdee27ad090
Utkarsh Kumar <107172738+utkarshKumar02@users.noreply.github.com>
2023-03-30 20:33:46 +0530

    Add files via upload

---
+++ functionpro.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int result = sum(a, b);
+        System.out.println(result);
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+ // Output: 15
